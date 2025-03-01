bf7ca78522edeca9fdca821a6a7f3d69d9c4e15c
ZachRadaza <zachradaza@gmail.com>
2025-03-01 13:37:38 -0800

    Add files via upload

--- CSC 130 Final/src/Main/Main.java
+++ CSC 130 Final/src/Main/Main.java
@@ -1,26 +1,25 @@
 package Main;
 
 import java.awt.Color;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
 
 import Data.Vector2D;
 import logic.Control;
 import timer.stopWatchX;
+import Data.spriteInfo;
 
 public class Main{
 	// Fields (Static) below...
 	public static Color darkGreen = new Color(0, 100, 50);
-	public static stopWatchX timer = new stopWatchX(1);
-	public static stopWatchX timerFrames = new stopWatchX(50);
+	public static stopWatchX timer = new stopWatchX(50);
+	public static stopWatchX timerSquare = new stopWatchX(50);
 	
-	public static Queue<Vector2D> vecs1 = new LinkedList<>();
-	public static Queue<Vector2D> vecs2 = new LinkedList<>();
+	public static ArrayList<spriteInfo> sprites = new ArrayList<>();
+	public static int currentSpriteIndex = 0;
 	
-	public static Vector2D currentVec = new Vector2D(-100, -100);
-	public static boolean vecs1InUse = true;
-	public static int frameUsingN = 0; //decides which frame to use, needs frameUsing Method
-	public static String frameUsingHolder = "Ex0";
+	//personal testing
+	public static ArrayList<spriteInfo> spritesSquare = new ArrayList<>();
+	public static int currentSpriteIndexSquare = 0;
 	// End Static fields...
 	
 	public static void main(String[] args) {
@@ -31,11 +30,20 @@ public class Main{
 	/* This is your access to things BEFORE the game loop starts */
 	public static void start(){
 		// TODO: Code your starting conditions here...NOT DRAW CALLS HERE! (no addSprite or drawString)
+		String[] frameInUse = {"ChelseyEast1", "ChelseyEast2", "ChelseyEast2", "ChelseyEast2", "ChelseyEast1", "ChelseyEast3", "ChelseyEast3", "ChelseyEast3"};
+		int i = 0;
 		
-		currentVec.setY(300);
-		for(int n = currentVec.getX(); n < 1350; n += 10){
-			vecs1.add(new Vector2D(n,300));
+		for(int n = -100; n < 1350; n += 10){
+			sprites.add(new spriteInfo(new Vector2D(n, 300), frameInUse[i]));
+			i++;
+			if(i == 8) i = 0;
 		}
+		
+		//personal testing
+		//loadCoordsTag(400, 500, "ChelseyEast1", "ChelseyEast2", "ChelseyEast3", false, true);
+		//loadCoordsTag(700, 500, "ChelseyNorth1", "ChelseyNorth2", "ChelseyNorth3", false, false);
+		//loadCoordsTag(700, 200, "ChelseyWest1", "ChelseyWest2", "ChelseyWest3", true, true);
+		//loadCoordsTag(400, 200, "ChelseySouth1", "ChelseySouth2", "ChelseySouth3", true, false);
 	}
 		
 	
@@ -43,57 +51,67 @@ public class Main{
 	/* This is your access to the "game loop" (It is a "callback" method from the Control class (do NOT modify that class!))*/
 	public static void update(Control ctrl) {
 		// TODO: This is where you can code! (Starting code below is just to show you how it works)
-		ctrl.addSpriteToFrontBuffer(1060, 570, "ChelseySouthBase");						 				// Add a tester sprite to render list by tag (Remove later! Test only!)		
+		ctrl.addSpriteToFrontBuffer(1060, 570, "ChelseySouth1");						 				// Add a tester sprite to render list by tag (Remove later! Test only!)		
+
+		ctrl.drawString(1000, 710, "Zachary Juls Radaza", darkGreen);		// Test drawing text on screen where you want (Remove later! Test only!)
 		
-		if(timerFrames.isTimeUp()){
-			frameUsingHolder = frameUsing();
-			timerFrames.resetWatch();
-		}
-		
-		ctrl.drawString(1000, 710, "Zachary Juls Aballe Radaza", darkGreen);		// Test drawing text on screen where you want (Remove later! Test only!)
-		
-		ctrl.addSpriteToFrontBuffer(currentVec.getX(), currentVec.getY(), frameUsingHolder);
-		
-		
-		if(timer.isTimeUp() && vecs1InUse){
-			currentVec.setX(vecs1.peek().getX());
-			currentVec.setY(vecs1.peek().getY());
-			vecs2.add(vecs1.remove());
+		ctrl.addSpriteToFrontBuffer(sprites.get(currentSpriteIndex).getCoords().getX(), sprites.get(currentSpriteIndex).getCoords().getY(), sprites.get(currentSpriteIndex).getTag());
+
+		while(timer.isTimeUp()){
+			currentSpriteIndex++;
+			if(currentSpriteIndex >= sprites.size() - 1) currentSpriteIndex = 0;
 			timer.resetWatch();
-			if(vecs1.isEmpty()) vecs1InUse = false;
-		}
-		if(timer.isTimeUp() && !vecs1InUse){
-			currentVec.setX(vecs2.peek().getX());
-			currentVec.setY(vecs2.peek().getY());
-			vecs1.add(vecs2.remove());
-			timer.resetWatch();
-			if(vecs2.isEmpty()) vecs1InUse = true;
 		}
 		
+		//personal testing, testing all frames
+		/*
+		ctrl.addSpriteToFrontBuffer(spritesSquare.get(currentSpriteIndexSquare).getCoords().getX(), spritesSquare.get(currentSpriteIndexSquare).getCoords().getY(), spritesSquare.get(currentSpriteIndexSquare).getTag());
 		
+		while(timerSquare.isTimeUp()){
+			currentSpriteIndexSquare++;
+			if(currentSpriteIndexSquare >= spritesSquare.size() - 1) currentSpriteIndexSquare = 0;
+			timerSquare.resetWatch();
+		}
+		*/
 	}
 	
 	// Additional Static methods below...(if needed)
-	//determine which frame to use
-	public static String frameUsing(){
-		frameUsingN++;
-		if(frameUsingN == 6) frameUsingN = 1;
-		switch(frameUsingN - 1){
-			case 0:
-				return "Ex0";
-			case 1:
-				return "Ex1";
-			case 2:
-				return "Ex2";
-			case 3:
-				return "Ex3";
-			case 4:
-				return "Ex2";
-			case 5:
-				return "Ex1";
-			default:
-				return "ChelseySouthBase";
-		}
-	}
+	
+	//personal testing
+	
+	private static void loadCoordsTag(int xStart, int yStart, String tag1, String tag2, String tag3, boolean travellingNegative, boolean usingX){
+		String[] frameInUse = {tag1, tag2, tag2, tag2, tag1, tag3, tag3, tag3};
+		int i = 0;
 
+		if(usingX){
+			if(!travellingNegative){
+				for(int j = xStart;j < xStart + 300;j += 10){
+					spritesSquare.add(new spriteInfo(new Vector2D(j, yStart), frameInUse[i]));
+					i++;
+					if(i == 8) i = 0;
+				}
+			} else {
+				for(int j = xStart;j > xStart - 300;j -= 10){
+					spritesSquare.add(new spriteInfo(new Vector2D(j, yStart), frameInUse[i]));
+					i++;
+					if(i == 8) i = 0;
+				}
+			}
+		} else {
+			if(!travellingNegative){
+				for(int j = yStart;j > yStart - 300;j -= 10){
+					spritesSquare.add(new spriteInfo(new Vector2D(xStart, j), frameInUse[i]));
+					i++;
+					if(i == 8) i = 0;
+				}
+			} else {
+				for(int j = yStart;j < yStart + 300;j += 10){
+					spritesSquare.add(new spriteInfo(new Vector2D(xStart, j), frameInUse[i]));
+					i++;
+					if(i == 8) i = 0;
+				}
+			}
+		}
+
+	}
 }
