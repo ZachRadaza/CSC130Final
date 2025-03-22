3e8ba6acb7026fe1d123cc8ea03fe3726cfd517c
ZachRadaza <zachradaza@gmail.com>
2025-03-21 19:55:40 -0700

    Add files via upload

--- CSC 130 Final/src/Main/Main.java
+++ CSC 130 Final/src/Main/Main.java
@@ -2,24 +2,27 @@ package Main;
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 import Data.Vector2D;
 import logic.Control;
 import timer.stopWatchX;
 import Data.spriteInfo;
+import FileIO.EZFileRead;
 
 public class Main{
 	// Fields (Static) below...
 	public static Color darkGreen = new Color(0, 100, 50);
 	public static stopWatchX timer = new stopWatchX(50);
-	public static stopWatchX timerSquare = new stopWatchX(50);
+	public static stopWatchX timerDialogue = new stopWatchX(5000);
 	
 	public static ArrayList<spriteInfo> sprites = new ArrayList<>();
 	public static int currentSpriteIndex = 0;
 	
-	//personal testing
-	public static ArrayList<spriteInfo> spritesSquare = new ArrayList<>();
-	public static int currentSpriteIndexSquare = 0;
+	public static HashMap<String, String> dialogue = new HashMap<>();
+	public static String[] dialogueKey = new String[5];
+	public static int currentDialogueKey = 0;
 	// End Static fields...
 	
 	public static void main(String[] args) {
@@ -39,11 +42,16 @@ public class Main{
 			if(i == 8) i = 0;
 		}
 		
-		//personal testing
-		//loadCoordsTag(400, 500, "ChelseyEast1", "ChelseyEast2", "ChelseyEast3", false, true);
-		//loadCoordsTag(700, 500, "ChelseyNorth1", "ChelseyNorth2", "ChelseyNorth3", false, false);
-		//loadCoordsTag(700, 200, "ChelseyWest1", "ChelseyWest2", "ChelseyWest3", true, true);
-		//loadCoordsTag(400, 200, "ChelseySouth1", "ChelseySouth2", "ChelseySouth3", true, false);
+		EZFileRead ezr = new EZFileRead("dialogueChelsey.txt");
+		String key = "";
+		String value = "";
+		for(int j = 0; j < ezr.getNumLines(); j++){
+			StringTokenizer tokenizer = new StringTokenizer(ezr.getNextLine(), "*");
+			key = tokenizer.nextToken();
+			value = tokenizer.nextToken();
+			dialogue.put(key, value);
+			dialogueKey[j] = key;
+		}
 	}
 		
 	
@@ -56,62 +64,23 @@ public class Main{
 		ctrl.drawString(1000, 710, "Zachary Juls Radaza", darkGreen);		// Test drawing text on screen where you want (Remove later! Test only!)
 		
 		ctrl.addSpriteToFrontBuffer(sprites.get(currentSpriteIndex).getCoords().getX(), sprites.get(currentSpriteIndex).getCoords().getY(), sprites.get(currentSpriteIndex).getTag());
+		
+		ctrl.drawString(100, 250, dialogue.get(dialogueKey[currentDialogueKey]), Color.WHITE);
 
 		while(timer.isTimeUp()){
 			currentSpriteIndex++;
 			if(currentSpriteIndex >= sprites.size() - 1) currentSpriteIndex = 0;
 			timer.resetWatch();
 		}
-		
-		//personal testing, testing all frames
-		/*
-		ctrl.addSpriteToFrontBuffer(spritesSquare.get(currentSpriteIndexSquare).getCoords().getX(), spritesSquare.get(currentSpriteIndexSquare).getCoords().getY(), spritesSquare.get(currentSpriteIndexSquare).getTag());
-		
-		while(timerSquare.isTimeUp()){
-			currentSpriteIndexSquare++;
-			if(currentSpriteIndexSquare >= spritesSquare.size() - 1) currentSpriteIndexSquare = 0;
-			timerSquare.resetWatch();
+
+		while(timerDialogue.isTimeUp()){
+			currentDialogueKey++;
+			if(currentDialogueKey >= dialogueKey.length) currentDialogueKey = 0;
+			timerDialogue.resetWatch();
 		}
-		*/
+		
 	}
 	
 	// Additional Static methods below...(if needed)
-	
-	//personal testing
-	
-	private static void loadCoordsTag(int xStart, int yStart, String tag1, String tag2, String tag3, boolean travellingNegative, boolean usingX){
-		String[] frameInUse = {tag1, tag2, tag2, tag2, tag1, tag3, tag3, tag3};
-		int i = 0;
 
-		if(usingX){
-			if(!travellingNegative){
-				for(int j = xStart;j < xStart + 300;j += 10){
-					spritesSquare.add(new spriteInfo(new Vector2D(j, yStart), frameInUse[i]));
-					i++;
-					if(i == 8) i = 0;
-				}
-			} else {
-				for(int j = xStart;j > xStart - 300;j -= 10){
-					spritesSquare.add(new spriteInfo(new Vector2D(j, yStart), frameInUse[i]));
-					i++;
-					if(i == 8) i = 0;
-				}
-			}
-		} else {
-			if(!travellingNegative){
-				for(int j = yStart;j > yStart - 300;j -= 10){
-					spritesSquare.add(new spriteInfo(new Vector2D(xStart, j), frameInUse[i]));
-					i++;
-					if(i == 8) i = 0;
-				}
-			} else {
-				for(int j = yStart;j < yStart + 300;j += 10){
-					spritesSquare.add(new spriteInfo(new Vector2D(xStart, j), frameInUse[i]));
-					i++;
-					if(i == 8) i = 0;
-				}
-			}
-		}
-
-	}
 }
