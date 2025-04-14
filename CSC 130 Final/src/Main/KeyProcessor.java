4fde4a8ba8dd25e7c8537480a629e681a61076c4
ZachRadaza <zachradaza@gmail.com>
2025-04-14 10:47:06 -0700

    Add files via upload

--- CSC 130 Final/src/Main/KeyProcessor.java
+++ CSC 130 Final/src/Main/KeyProcessor.java
@@ -8,12 +8,18 @@ import timer.stopWatchX;
 public class KeyProcessor{
 	// Static Fields
 	private static char last = ' ';			// For debouncing purposes
-	private static stopWatchX sw = new stopWatchX(250);
+	private static stopWatchX sw = new stopWatchX(5);
 	
+	public static int direction = 1; //direction character is going
 	// Static Method(s)
 	public static void processKey(char key){
-		if(key == ' ')				return;
+		/*
+		if(key == ' ')	{			
+			Main.trigger = "";
+			return;
+		}
 		// Debounce routine below...
+		*/
 		if(key == last)
 			if(sw.isTimeUp() == false)			return;
 		last = key;
@@ -23,6 +29,44 @@ public class KeyProcessor{
 		switch(key){
 		case '%':								// ESC key
 			System.exit(0);
+			break;
+			
+		case ' ':
+			Main.currentSpriteIndex = 0;
+			Main.currentlyMoving = false;
+			Main.trigger = "";
+			break;
+			
+		case 'w':
+			direction = 0;
+			Main.currentlyMoving = true;
+			Main.sprite.getCoords().adjustY(-5);
+			Main.trigger = "w has been triggered";
+			break;
+			
+		case 'a':
+			direction = 3;
+			Main.currentlyMoving = true;
+			Main.sprite.getCoords().adjustX(-5);
+			Main.trigger = "a has been triggered";
+			break;
+			
+		case 's':
+			direction = 1;
+			Main.currentlyMoving = true;
+			Main.sprite.getCoords().adjustY(5);
+			Main.trigger = "s has been triggered";
+			break;
+			
+		case 'd':
+			direction = 2;
+			Main.currentlyMoving = true;
+			Main.sprite.getCoords().adjustX(5);
+			Main.trigger = "d has been triggered";
+			break; 
+			
+		case '$':
+			Main.trigger = "spacebar has been triggered";
 			break;
 			
 		case 'm':
--- CSC 130 Final/src/Main/Main.java
+++ CSC 130 Final/src/Main/Main.java
@@ -1,7 +1,6 @@
 package Main;
 
 import java.awt.Color;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
@@ -14,15 +13,25 @@ import FileIO.EZFileRead;
 public class Main{
 	// Fields (Static) below...
 	public static Color darkGreen = new Color(0, 100, 50);
-	public static stopWatchX timer = new stopWatchX(50);
+	public static stopWatchX timer = new stopWatchX(68);
 	public static stopWatchX timerDialogue = new stopWatchX(5000);
 	
-	public static ArrayList<spriteInfo> sprites = new ArrayList<>();
-	public static int currentSpriteIndex = 0;
+	//frames in use, [set of frames][specific frame]
+	public static String[][] framesInUse = {
+											{"ChelseyNorth1", "ChelseyNorth2", "ChelseyNorth2", "ChelseyNorth2", "ChelseyNorth1", "ChelseyNorth3", "ChelseyNorth3", "ChelseyNorth3"},
+											{"ChelseySouth1", "ChelseySouth2", "ChelseySouth2", "ChelseySouth2", "ChelseySouth1", "ChelseySouth3", "ChelseySouth3", "ChelseySouth3"},
+											{"ChelseyEast1", "ChelseyEast2", "ChelseyEast2", "ChelseyEast2", "ChelseyEast1", "ChelseyEast3", "ChelseyEast3", "ChelseyEast3"},
+											{"ChelseyWest1", "ChelseyWest2", "ChelseyWest2", "ChelseyWest2", "ChelseyWest1", "ChelseyWest3", "ChelseyWest3", "ChelseyWest3"}
+											};
+	public static int currentSpriteIndex = 0; //current frame we are on
+	public static spriteInfo sprite = new spriteInfo(new Vector2D(610, 350), framesInUse[KeyProcessor.direction][currentSpriteIndex]);
+	public static boolean currentlyMoving = false;
 	
 	public static HashMap<String, String> dialogue = new HashMap<>();
 	public static String[] dialogueKey = new String[5];
 	public static int currentDialogueKey = 0;
+	
+	public static String trigger = "";
 	// End Static fields...
 	
 	public static void main(String[] args) {
@@ -33,14 +42,6 @@ public class Main{
 	/* This is your access to things BEFORE the game loop starts */
 	public static void start(){
 		// TODO: Code your starting conditions here...NOT DRAW CALLS HERE! (no addSprite or drawString)
-		String[] frameInUse = {"ChelseyEast1", "ChelseyEast2", "ChelseyEast2", "ChelseyEast2", "ChelseyEast1", "ChelseyEast3", "ChelseyEast3", "ChelseyEast3"};
-		int i = 0;
-		
-		for(int n = -100; n < 1350; n += 10){
-			sprites.add(new spriteInfo(new Vector2D(n, 300), frameInUse[i]));
-			i++;
-			if(i == 8) i = 0;
-		}
 		
 		EZFileRead ezr = new EZFileRead("dialogueChelsey.txt");
 		String key = "";
@@ -61,15 +62,15 @@ public class Main{
 		// TODO: This is where you can code! (Starting code below is just to show you how it works)
 		ctrl.addSpriteToFrontBuffer(1060, 570, "Chair");						 				// Add a tester sprite to render list by tag (Remove later! Test only!)		
 
-		ctrl.drawString(1000, 710, "Zachary Juls Radaza", darkGreen);		// Test drawing text on screen where you want (Remove later! Test only!)
+		ctrl.drawString(700, 360, trigger, darkGreen);		// Test drawing text on screen where you want (Remove later! Test only!)
 		
-		ctrl.addSpriteToFrontBuffer(sprites.get(currentSpriteIndex).getCoords().getX(), sprites.get(currentSpriteIndex).getCoords().getY(), sprites.get(currentSpriteIndex).getTag());
+		ctrl.addSpriteToFrontBuffer(sprite.getCoords().getX(), sprite.getCoords().getY(), framesInUse[KeyProcessor.direction][currentSpriteIndex]);
 		
 		ctrl.drawString(100, 250, dialogue.get(dialogueKey[currentDialogueKey]), Color.WHITE);
 
-		while(timer.isTimeUp()){
+		while(timer.isTimeUp() && currentlyMoving){
 			currentSpriteIndex++;
-			if(currentSpriteIndex >= sprites.size() - 1) currentSpriteIndex = 0;
+			if(currentSpriteIndex >= framesInUse[0].length - 1) currentSpriteIndex = 0;
 			timer.resetWatch();
 		}
 
